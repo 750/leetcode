c3e07f83acbdada3cb6aeb417d0561c9eb8758ea
saprolino <saprolino@Gmail.com>
2021-07-04 21:33:16 +0300

    Added 5

---
+++ 5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        s = "!"+"!".join(s)+"!"
+        deltas = [-1 for i in range(len(s))]
+        deltas[0] = 0
+        biggest_center = 0
+        res = 0
+        for center in range(0, len(s)):
+            # mirror
+            mirror = 2*biggest_center-center
+            
+            start = 0
+            if center <= biggest_center+deltas[biggest_center]:
+                start = min(deltas[mirror], biggest_center+deltas[biggest_center]-center)
+            
+            while center-start >= 0 and center+start < len(s):
+                if s[center+start] == s[center-start]:
+                    start+=1
+                else:
+                    break
+            
+            start -= 1
+            if center+start >= biggest_center+deltas[biggest_center]:
+                biggest_center = center
+            
+            if start > deltas[res]:
+                res = center
+            deltas[center] = start
+            
+        return ''.join([i for i in s[res-deltas[res]:res+deltas[res]+1] if i != "!"])
